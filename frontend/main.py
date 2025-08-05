74f359c5c950cbe7342bbbd7cb4402fff0376993
carlos gaviria <ing.carlos.gaviria@gmail.com>
2025-08-05 12:05:05 -0500

    se agrega registrar

--- frontend/main.py
+++ frontend/main.py
@@ -1,20 +1,49 @@
+
 import flet as ft
 import requests
 
-API_URL = "http://127.0.0.1:8000/"  # Apunta al backend
+API_URL = "http://127.0.0.1:8000/api/"
 
 def main(page: ft.Page):
-    page.title = "Flet + DRF"
+    page.title = "Flet + DRF Auth"
     page.scroll = "auto"
 
-    def enviar(e):
-        r = requests.get(API_URL)
-        resultado.value = f"Respuesta: {r.status_code}"
+    username = ft.TextField(label="Usuario")
+    password = ft.TextField(label="Contraseña", password=True, can_reveal_password=True)
+    resultado = ft.Text()
+
+    def login(e):
+        r = requests.post(API_URL + "login/", json={
+            "username": username.value,
+            "password": password.value
+        })
+        if r.status_code == 200:
+            resultado.value = f"Login OK. Token: {r.json()['access']}"
+        else:
+            resultado.value = f"Error: {r.json().get('detail', r.text)}"
         page.update()
 
-    boton = ft.ElevatedButton("Probar API", on_click=enviar)
-    resultado = ft.Text()
-    
-    page.add(ft.Column([boton, resultado]))
+    def register(e):
+        r = requests.post(API_URL + "register/", json={
+            "username": username.value,
+            "password": password.value
+        })
+        if r.status_code == 201:
+            resultado.value = "Usuario registrado correctamente"
+        else:
+            resultado.value = f"Error: {r.json().get('error', r.text)}"
+        page.update()
+
+    page.add(
+        ft.Column([
+            username,
+            password,
+            ft.Row([
+                ft.ElevatedButton("Login", on_click=login),
+                ft.ElevatedButton("Registrar", on_click=register),
+            ]),
+            resultado
+        ])
+    )
 
 ft.app(target=main)
